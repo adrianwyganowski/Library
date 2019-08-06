5820598504391510b10a2437e172d2ed6fc1f075
adrianwyganowski <52456597+adrianwyganowski@users.noreply.github.com>
2019-08-06 13:52:42 +0200

    working on not mobile nav

--- src/js/navbar_mobile.js
+++ src/js/navbar_mobile.js
@@ -1,28 +1,57 @@
 const NavbarMobile = (function() {
     'use strict';
     var DOM ={
+        navbarMobileIsOn: false,
         navbar : document.getElementsByClassName('navigation')[0],
         container : document.getElementsByClassName('container')[0],
         hamburgerButton : document.getElementsByClassName('hamburgerButton')[0],
         navExitButton : document.getElementsByClassName('navigation__list--exitButton')[0],
     };
-   function addEventListners(){
+    (function(){
+        if (screen.width < 900){
+            DOM.navbarMobileIsOn = true;
+        }
+        checkSizeOfWindow();
+    }());
+    function eventCheckSizeOfWindow(){
+        window.addEventListener('resize',checkSizeOfWindow);
+    }
+    function checkSizeOfWindow(){
+        if (screen.width < 900 && DOM.navbarMobileIsOn === true){
+            addEventListners();
+            DOM.navbarMobileIsOn = false;
+            DOM.navbar.style.width = '0';
+            DOM.navbar.style.height = '100%';
+            console.log('tutaj');
+        }
+        else if (screen.width > 900 && DOM.navbarMobileIsOn === false) {
+            removeEventListners();
+            DOM.navbarMobileIsOn = true;
+            DOM.navbar.style.width = '100%';
+            DOM.navbar.style.height = '4rem';
+            DOM.container.style.marginLeft = '0';
+        }
+    }
+    function addEventListners(){
        DOM.hamburgerButton.addEventListener("click", openMenu);
        DOM.navExitButton.addEventListener("click", closeMenu);
    }
-   function openMenu(){
+   function removeEventListners(){
+        DOM.hamburgerButton.removeEventListener("click", openMenu);
+        DOM.navExitButton.removeEventListener("click", closeMenu);
+   }
+    function openMenu(){
         DOM.navbar.style.width = '10rem';
-        DOM.container.style.marginLeft = '10rem';
-           
+        DOM.container.style.marginLeft = '10rem'; 
     }
-   function closeMenu(){
+    function closeMenu(){
         DOM.navbar.style.width = '0rem';
         DOM.container.style.marginLeft = '0rem';
     }
-   function init() {
-        addEventListners();
-   }
-   return {
+    function init() {
+        eventCheckSizeOfWindow();
+    }
+    return {
        init: init,
-   };
+    };
 }());
