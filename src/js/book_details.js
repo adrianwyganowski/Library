a0dfdcd9ae589914e5b618761aaea248a1cbbd6d
adrianwyganowski <52456597+adrianwyganowski@users.noreply.github.com>
2019-09-17 11:57:14 +0200

    added validation for fetched data

--- src/js/book_details.js
+++ src/js/book_details.js
@@ -1,69 +1,86 @@
 "use strict";
 MYAPP.BookDetails = (function () {
-    let link = 'https://www.googleapis.com/books/v1/volumes?q=';
-    let key = '&AIzaSyDHcMuGxU4GazERRB-JIXDJjMm40qXt644';
+    const link = 'https://www.googleapis.com/books/v1/volumes?q=';
+    const key = '&AIzaSyDHcMuGxU4GazERRB-JIXDJjMm40qXt644';
     
     async function displayBook(book){
-        let reuqest = link + book + key;
-        let response = await fetch(reuqest);
-        let obj = await response.json();
-        let authorOfSpecificBook;
-        if (typeof obj.items[0].volumeInfo.authors == "undefined"){
-            authorOfSpecificBook = '';
-            displayRecommendedBooks(obj.items[0].volumeInfo.categories[0]);
-        }
-        else{
-            authorOfSpecificBook =obj.items[0].volumeInfo.authors[0];
-            displayRecommendedBooks(authorOfSpecificBook);
-        }
-        let div = document.createElement('div');
+        const reuqest = link + book + key;
+        const response = await fetch(reuqest);
+        const obj = await response.json();
+        const author = checkIfDataIsNull(obj.items[0].volumeInfo.authors[0]);
+        const imgSrc = checkIfDataIsNull(obj.items[0].volumeInfo.imageLinks.thumbnail);
+        const title = checkIfDataIsNull(obj.items[0].volumeInfo.title);
+        const description = checkIfDataIsNull(obj.items[0].volumeInfo.description);
+        const price = checkIfDataIsNull(obj.items[0].volumeInfo.pageCount);
+        const div = document.createElement('div');
         div.className = 'specificBook';
         div.innerHTML = 
         `<div class="specificBook__bookCover"> 
-            <img src='${obj.items[0].volumeInfo.imageLinks.thumbnail}' alt='Thumbnail photo of "${obj.items[0].volumeInfo.title}"'>
+            <img src='${imgSrc}' alt='Thumbnail photo of "${title}"'>
         </div>
         <main class='specificBook__information'>
             <div class="specificBook__information--name">
-                <h2>${obj.items[0].volumeInfo.title}</h2>
+                <h2>${title}</h2>
             </div>
             <div class='specificBook__information--author'>
-                <h4>${authorOfSpecificBook}</h4>
+                <h4>${author}</h4>
             </div>
             <div class='specificBook__information--description'>
-                <p>${obj.items[0].volumeInfo.description}<p> 
+                <p>${description}<p> 
             </div>
             <div class='specificBook__information--price'>
-                ${obj.items[0].volumeInfo.pageCount / 5} zł
+                ${price / 5} zł
             </div>
         </main>`
         document.getElementsByClassName('mainPage__books')[0].appendChild(div);
+        displayRecommendedBooks(author)
     }
     async function displayRecommendedBooks(connection){
-        let reuqest = link + connection + key;
-        let response = await fetch(reuqest);
-        let obj = await response.json();
+        const reuqest = link + connection + key;
+        const response = await fetch(reuqest);
+        const obj = await response.json();
        
         for (let i = 0; i < 3;i++){
-            let div = document.createElement('div');
+            const div = document.createElement('div');
             div.className = 'recommendedBook'
             div.addEventListener('click', function(){
                 sessionStorage.setItem('isbn',`${obj.items[i].volumeInfo.industryIdentifiers[0].identifier}`);
                 window.open('book.html','_self') ;
             })
+            const author = checkIfDataIsNull(obj.items[i].volumeInfo.authors[0]);
+            const imgSrc = checkIfImgSrcIsNull(obj.items[i].volumeInfo.imageLinks);
+            const title = checkIfDataIsNull(obj.items[i].volumeInfo.title);
             div.innerHTML = 
-            `<a href='#'>
-                <img src='${obj.items[i].volumeInfo.imageLinks.thumbnail}' alt='Thumbnail photo of "${obj.items[i].volumeInfo.title}"'>
+            `<a>
+                <img src='${imgSrc}' alt='Thumbnail photo of "${title}"'>
                 <div class='bookThumbnail__title'>
-                    ${obj.items[i].volumeInfo.title}
+                    ${title}
                 </div>
                 <div bookThumbnail__author>
-                    ${obj.items[i].volumeInfo.authors[0] }
+                    ${author}
                 </div>
             </a>`
             document.getElementsByClassName('mainPageAside__generes')[0].appendChild(div);
         }
     }
-   
+    function checkIfDataIsNull(data){
+        if (typeof data == "undefined"){
+            let result = '';
+            return result;
+        }
+        else{
+            return data;
+        }
+    }
+    function checkIfImgSrcIsNull(data){
+        if (typeof data == "undefined"){
+            let result = '';
+            return result;
+        }
+        else{
+            return data.thumbnail;
+        }
+    }
     function bookForBookDetails(book){
         displayBook(book);
     }
