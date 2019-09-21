4a75caa26c85c795e3db076613f4fd63d148ff90
adrianwyganowski <52456597+adrianwyganowski@users.noreply.github.com>
2019-09-21 14:17:29 +0200

    added new js file to display books from cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,8 +2,11 @@
 MYAPP.Cart = (function() {
 
     function buttonMutationObserver(){
-        
-        const target = document.getElementsByClassName('mainPage__book')[0];
+        if (document.getElementsByClassName('mainPage__book').length === 0)
+        {
+            return;
+        }
+        const target = document.getElementsByClassName('mainPage__book')[0]
         const config = { attributes: true, childList: true, subtree: true };
         const observer = new MutationObserver(buttonEvent);
         observer.observe(target,config);
@@ -12,12 +15,34 @@ MYAPP.Cart = (function() {
         const button = document.getElementsByClassName('specificBook__information--buyButton')[0];
         button.addEventListener('click',purchaseBook);
     }
+    function checkIfCartExist(){
+        if (sessionStorage.getItem('cart') !== null){
+            const result = sessionStorage.getItem('cart')
+            return JSON.parse(result);
+        }
+        else{
+            return [];
+        }
+    }
     function purchaseBook(){
         const bookISBN = sessionStorage.getItem('isbn');
-        console.log(bookISBN);
+        const cart = checkIfCartExist();
+        const cartWithPurchasedBook = `${cart}${bookISBN},`;
+        sessionStorage.setItem('cart', JSON.stringify(cartWithPurchasedBook));
+        howManyItemsInCart();
+    }
+    function howManyItemsInCart(){
+        const cart =  JSON.parse(sessionStorage.getItem('cart'));
+        if(cart !== null){
+            const cartArray = cart.split(','); 
+            const cartArrayLength = cartArray.length - 1  // to fix added one because of ',' 
+            const cartInMenu = document.getElementById('cartMenu');
+            cartInMenu.innerHTML = `Cart (${cartArrayLength})`;
+        }
     }
     function init(){
         buttonMutationObserver();
+        howManyItemsInCart();
     }
     return {
         init :init,
